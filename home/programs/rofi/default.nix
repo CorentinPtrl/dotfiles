e4edf6e2ceaf776e2c19538da5fef2df0fd6b926
Corentin.PITREL <gomarco@outlook.fr>
2023-07-16 15:26:40 +0200

    Move config to theme

--- home/programs/rofi/default.nix
+++ home/programs/rofi/default.nix
@@ -1,15 +1,12 @@
 {config, pkgs, ...}: {
   programs.rofi = {
     enable = true;
+    theme = ./config.rasi;
   };
 
   home.file = {
       ".config/rofi/fonts.rasi" = {
         source = ./fonts.rasi;
-        recursive = true;
-    };
-      ".config/rofi/config.rasi" = {
-        source = ./config.rasi;
         recursive = true;
     };
       ".config/rofi/colors.rasi" = {
