cea46cabf3cd5e8a58275a0adbd64511055458fc
Corentin.PITREL <gomarco@outlook.fr>
2023-07-16 15:36:40 +0200

    Change hyprland theme

--- home/programs/hypr/default.nix
+++ home/programs/hypr/default.nix
@@ -35,119 +35,148 @@
     nvidiaPatches = true;
     extraConfig = ''
 
-    # Monitor
-    monitor=DP-1,1920x1080@165,auto,1
-
-    # Fix slow startup
-    exec systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
-    exec dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY XDG_CURRENT_DESKTOP 
-
-    # Autostart
-
-    exec-once = hyprctl setcursor Bibata-Modern-Classic 24
-
-    exec = pkill waybar & sleep 0.5 && waybar
-    exec-once = swww init && swww img /home/corentin/Imagens/wallpapers/menhera.jpg
-
-    # Set en layout at startup
-
-    # Input config
-    input {
-        kb_layout = fr
-        kb_variant =
-        kb_model =
-        kb_options =
-        kb_rules =
-
-        follow_mouse = 1
-
-        touchpad {
-            natural_scroll = false
-        }
-
-        sensitivity = 0 # -1.0 - 1.0, 0 means no modification.
-    }
-
-    general {
-
-        gaps_in = 5
-        gaps_out = 20
-        border_size = 2
-        col.active_border = rgba(33ccffee) rgba(00ff99ee) 45deg
-        col.inactive_border = rgba(595959aa)
-
-        layout = dwindle
-    }
-
-    decoration {
-
-        rounding = 10
-        blur = true
-        blur_size = 3
-        blur_passes = 1
-        blur_new_optimizations = true
-
-        drop_shadow = true
-        shadow_range = 4
-        shadow_render_power = 3
-        col.shadow = rgba(1a1a1aee)
-    }
-
-    animations {
-        enabled = yes
-
-        bezier = ease,0.4,0.02,0.21,1
-
-        animation = windows, 1, 3.5, ease, slide
-        animation = windowsOut, 1, 3.5, ease, slide
-        animation = border, 1, 6, default
-        animation = fade, 1, 3, ease
-        animation = workspaces, 1, 3.5, ease
-    }
-
-    dwindle {
-        pseudotile = yes
-        preserve_split = yes
-    }
-
-    master {
-        new_is_master = yes
-    }
-
-    gestures {
-        workspace_swipe = false
-    }
-
-    # Example windowrule v1
-    # windowrule = float, ^(kitty)$
-    # Example windowrule v2
-    # windowrulev2 = float,class:^(kitty)$,title:^(kitty)$
-
-    windowrule=float,^(kitty)$
-    windowrule=float,^(pavucontrol)$
-    windowrule=center,^(kitty)$
-    windowrule=float,^(blueman-manager)$
-    windowrule=size 1040 670,^(kitty)$
-    windowrule=size 934 525,^(mpv)$
-    windowrule=float,^(mpv)$
-    windowrule=center,^(mpv)$
-    #windowrule=pin,^(firefox)$
-
-    $mainMod = CTRL
-    bind=$mainMod,E,killactive
-    bind=$mainMod,B,exec,librewolf
-    #bind=$mainMod,F,fullscreen,1
-    bind=$mainModSHIFT,F,fullscreen,0
-    bind=$mainMod,RETURN,exec,kitty
-    bind=SUPER,C,killactive,
-    #bind=$mainModSHIFT,Q,exit,
-    #bind=$mainMod,E,exec,pcmanfm
-    bind=$mainModSHIFT,D,exec, rofi -show drun
-    bind=$mainMod,P,pseudo,
-    bind=$mainMod,ESCAPE,exec,sudo systemctl suspend
+   <########################################################################################
+ __  __ _       _                 _ 
+|  \/  (_)_ __ (_)_ __ ___   __ _| |
+| |\/| | | '_ \| | '_ ` _ \ / _` | |
+| |  | | | | | | | | | | | | (_| | |
+|_|  |_|_|_| |_|_|_| |_| |_|\__,_|_|
+                                    
+ _   _                  _                 _    ____             __ _           
+| | | |_   _ _ __  _ __| | __ _ _ __   __| |  / ___|___  _ __  / _(_) __ _ ___ 
+| |_| | | | | '_ \| '__| |/ _` | '_ \ / _` | | |   / _ \| '_ \| |_| |/ _` / __|
+|  _  | |_| | |_) | |  | | (_| | | | | (_| | | |__| (_) | | | |  _| | (_| \__ \
+|_| |_|\__, | .__/|_|  |_|\__,_|_| |_|\__,_|  \____\___/|_| |_|_| |_|\__, |___/
+       |___/|_|                                                      |___/    
+#########################################################################################
 
 
-  bindle=,XF86MonBrightnessUp,exec,~/.config/hypr/scripts/brightness up  # increase screen brightness
+$mainMod=CTRL
+
+#monitor=eDP-1,1920x1080@60,0x0,1
+#Xdg desktop
+exec-once=~/script/start-xdg-desktop.sh
+# Status bar :) 
+#exec-once=waybar
+#Notification 
+exec-once=dunst
+# Wallpaper
+#exec-once=swaybg -o \* -i ~/.config/hypr/wallpapers/tree.jpg -m fill
+# For screen sharing 
+exec-once=dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
+# For keyboard 
+exec-once=fcitx5 -D
+# For lockscreen
+exec-once=swayidle -w timeout 2000 'swaylock' before-sleep 'swaylock -f'
+# Start Page
+exec-once=~/.config/hypr/scripts/startpage.sh
+
+#Auth agent
+exec-once=/usr/lib/polkit-kde-authentication-agent-1
+
+# Bluetooth
+exec-once=blueman-applet # Make sure you have installed blueman
+
+# Screen Sharing 
+exec-once=systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
+exec-once=~/.config/hypr/scripts/screensharing.sh
+
+
+input {
+  kb_layout=fr
+  #Remap Capslock -> Esc for Vim users  
+  #kb_options=caps:escape 
+  repeat_rate=50
+  repeat_delay=240
+
+  touchpad {
+    disable_while_typing=1
+    natural_scroll=1
+    #clickfinger_behavior=1
+    middle_button_emulation=0
+    tap-to-click=1
+  }
+}
+
+
+gestures { 
+  workspace_swipe=true 
+  workspace_swipe_min_speed_to_force=5
+}
+
+misc {
+  disable_hyprland_logo=true
+  disable_splash_rendering=true
+  mouse_move_enables_dpms=true
+  #no_vfr=1
+}
+
+########################################################################################
+
+\ \        / (_)         | |                   |  __ \     | |          
+  \ \  /\  / / _ _ __   __| | _____      _____  | |__) |   _| | ___  ___ 
+   \ \/  \/ / | | '_ \ / _` |/ _ \ \ /\ / / __| |  _  / | | | |/ _ \/ __|
+    \  /\  /  | | | | | (_| | (_) \ V  V /\__ \ | | \ \ |_| | |  __/\__ \
+     \/  \/   |_|_| |_|\__,_|\___/ \_/\_/ |___/ |_|  \_\__,_|_|\___||___/
+
+########################################################################################
+
+
+# Float Necessary Windows
+windowrule=float,Rofi
+windowrule=float,pavucontrol
+windowrulev2 = float,class:^()$,title:^(Picture in picture)$
+windowrulev2 = float,class:^(brave)$,title:^(Save File)$
+windowrulev2 = float,class:^(brave)$,title:^(Open File)$
+windowrulev2 = float,class:^(LibreWolf)$,title:^(Picture-in-Picture)$
+windowrulev2 = float,class:^(blueman-manager)$
+
+# Increase the opacity 
+windowrule=opacity 0.92,Thunar
+windowrule=opacity 0.96,discord
+windowrule=opacity 0.9,VSCodium
+windowrule=opacity 0.88,obsidian
+windowrulev2=opacity 0.96,class:^(notion-nativefier-46aee8)$
+windowrulev2=opacity 0.80,class:^(Spotify)$
+^.*nvim.*$
+windowrule=tile,librewolf
+windowrule=tile,spotify
+windowrule=opacity 1,neovim
+bindm=ALT,mouse:272,movewindow
+bindm=ALT,mouse:273,resizewindow
+
+
+###########################################
+  ____  _           _ _                 
+ |  _ \(_)         | (_)                
+ | |_) |_ _ __   __| |_ _ __   __ _ ___ 
+ |  _ <| | '_ \ / _` | | '_ \ / _` / __|
+ | |_) | | | | | (_| | | | | | (_| \__ \
+ |____/|_|_| |_|\__,_|_|_| |_|\__, |___/
+                               __/ |    
+                              |___/     
+
+###########################################
+
+# example binds
+bind=$mainMod,E,killactive
+bind=$mainMod,B,exec,librewolf
+#bind=$mainMod,F,fullscreen,1
+bind=$mainModSHIFT,F,fullscreen,0
+bind=$mainMod,RETURN,exec,kitty
+bind=SUPER,C,killactive,
+#bind=$mainModSHIFT,Q,exit,
+#bind=$mainMod,E,exec,pcmanfm
+bind=$mainModSHIFT,D,exec, rofi -show drun
+bind=$mainMod,P,pseudo,
+bind=$mainMod,ESCAPE,exec,sudo systemctl suspend
+
+bind=,XF86AudioMute,exec,~/.config/hypr/scripts/volume mute
+bind=,XF86AudioLowerVolume,exec,~/.config/hypr/scripts/volume down
+bind=,XF86AudioRaiseVolume,exec,~/.config/hypr/scripts/volume up
+bind=,XF86AudioMicMute,exec,pactl set-source-mute @DEFAULT_SOURCE@ toggle
+
+bindle=,XF86MonBrightnessUp,exec,~/.config/hypr/scripts/brightness up  # increase screen brightness
 bindle=,XF86MonBrightnessDown,exec,~/.config/hypr/scripts/brightness down # decrease screen brightnes
 
 
@@ -156,58 +185,120 @@ bindle=,XF86MonBrightnessDown,exec,~/.config/hypr/scripts/brightness down # decr
 bind=$mainModSHIFT,E,exec,wlogout
 bind =CTRL, T, togglefloating,
 bind=$mainModSHIFT,P,exec,pomotroid --in-process-gpu
-    # Functional keybinds
-    bind =,XF86AudioMicMute,exec,pamixer --default-source -t
-    bind =,XF86MonBrightnessDown,exec,light -U 20
-    bind =,XF86MonBrightnessUp,exec,light -A 20
-    bind =,XF86AudioMute,exec,pamixer -t
-    bind =,XF86AudioLowerVolume,exec,pamixer -d 10
-    bind =,XF86AudioRaiseVolume,exec,pamixer -i 10
-    bind =,XF86AudioPlay,exec,playerctl play-pause
-    bind =,XF86AudioPause,exec,playerctl play-pause
 
-    # to switch between windows in a floating workspace
-    bind = SUPER,Tab,cyclenext,
-    bind = SUPER,Tab,bringactivetotop,
+# Screen shot 
+bind=ALT,R,exec,grim -g "$(slurp)" - | wl-copy
+# Screen recorder
+bind=$mainModALT,R,exec,wf-recorder -g "$(slurp)"
+# Emoji selector 
+#bind=$mainMod,E,exec,rofi -modi emoji -show emoji
 
-    # Move focus with mainMod + arrow keys
-    bind = $mainMod, left, movefocus, l
-    bind = $mainMod, right, movefocus, r
-    bind = $mainMod, up, movefocus, u
-    bind = $mainMod, down, movefocus, d
 
-    # Switch workspaces with mainMod + [0-9]
-    bind = $mainMod, 1, workspace, 1
-    bind = $mainMod, 2, workspace, 2
-    bind = $mainMod, 3, workspace, 3
-    bind = $mainMod, 4, workspace, 4
-    bind = $mainMod, 5, workspace, 5
-    bind = $mainMod, 6, workspace, 6
-    bind = $mainMod, 7, workspace, 7
-    bind = $mainMod, 8, workspace, 8
-    bind = $mainMod, 9, workspace, 9
-    bind = $mainMod, 0, workspace, 10
+bind=$mainModSHIFT,RETURN,layoutmsg,swapwithmaster
+bind=$mainMod,j,layoutmsg,cyclenext
+bind=$mainMod,k,layoutmsg,cycleprev
 
-    # Move active window to a workspace with mainMod + SHIFT + [0-9]
-    bind = $mainMod SHIFT, 1, movetoworkspace, 1
-    bind = $mainMod SHIFT, 2, movetoworkspace, 2
-    bind = $mainMod SHIFT, 3, movetoworkspace, 3
-    bind = $mainMod SHIFT, 4, movetoworkspace, 4
-    bind = $mainMod SHIFT, 5, movetoworkspace, 5
-    bind = $mainMod SHIFT, 6, movetoworkspace, 6
-    bind = $mainMod SHIFT, 7, movetoworkspace, 7
-    bind = $mainMod SHIFT, 8, movetoworkspace, 8
-    bind = $mainMod SHIFT, 9, movetoworkspace, 9
-    bind = $mainMod SHIFT, 0, movetoworkspace, 10
+bind=$mainMod,h,movefocus,l
+bind=$mainMod,l,movefocus,r
 
-    # Scroll through existing workspaces with mainMod + scroll
-    bind = $mainMod, mouse_down, workspace, e+1
-    bind = $mainMod, mouse_up, workspace, e-1
+bind=ALT,left,resizeactive,-40 0
+bind=ALT,right,resizeactive,40 0
 
-    # Move/resize windows with mainMod + LMB/RMB and dragging
-    bindm = $mainMod, mouse:272, movewindow
-    bindm = $mainMod, mouse:273, resizewindow
-    bindm = ALT, mouse:272, resizewindow
-        '';
+bind=ALT,up,resizeactive,0 -40
+bind=ALT,down,resizeactive,0 40
+
+bind=$mainModSHIFT,h,movewindow,l
+bind=$mainModSHIFT,l,movewindow,r
+bind=$mainModSHIFT,k,movewindow,u
+bind=$mainModSHIFT,j,movewindow,d
+
+bind=$mainMod,ampersand,workspace,1
+bind=$mainMod,eacute,workspace,2
+bind=$mainMod,quotedbl,workspace,3
+bind=$mainMod,apostrophe,workspace,4
+bind=$mainMod,parenleft,workspace,5
+bind=$mainMod,minus,workspace,6
+bind=$mainMod,egrave,workspace,7
+bind=$mainMod,underscore,workspace,8
+bind=$mainMod,ccedilla,workspace,9
+bind=$mainMod,agrave,workspace,10
+
+bind=$mainModSHIFT,1,movetoworkspacesilent,1
+bind=$mainModSHIFT,2,movetoworkspacesilent,2
+bind=$mainModSHIFT,3,movetoworkspacesilent,3
+bind=$mainModSHIFT,4,movetoworkspacesilent,4
+bind=$mainModSHIFT,5,movetoworkspacesilent,5
+bind=$mainModSHIFT,6,movetoworkspacesilent,6
+bind=$mainModSHIFT,7,movetoworkspacesilent,7
+bind=$mainModSHIFT,8,movetoworkspacesilent,8
+bind=$mainModSHIFT,9,movetoworkspacesilent,9
+bind=$mainModSHIFT,0,movetoworkspacesilent,10
+
+$THEME=iamverysimple
+
+$THEME_DIR=$HOME/.config/hypr/themes/iamverysimple
+
+monitor=,addreserved,55,0,0,0
+# Status bar :) 
+#exec=killall -3 eww & sleep 1 && "$THEME_DIR"/eww/launch_bar
+#Notification 
+exec-once=dunst
+# Wallpaper
+exec=pkill -9 swwwpaper ; "$THEME_DIR"/scripts/swwwpaper "$THEME_DIR"/wallpaper/
+exec-once=swayidle -w timeout 200 'swaylock-fancy'
+
+general {
+    layout=dwindle
+    sensitivity=1.0 # for mouse cursor
+
+    gaps_in=5
+    gaps_out=20
+    border_size=2
+    col.active_border=0xff5e81ac
+    col.inactive_border=0x66333333
+    apply_sens_to_raw=0 # whether to apply the sensitivity to raw input (e.g. used by>
+}
+
+decoration {
+    rounding=13
+    blur=1
+    blur_size=6.8 # minimum 1
+    blur_passes=2 # minimum 1, more passes = more resource intensive.
+    blur_new_optimizations = true
+    # Your blur "amount" is blur_size * blur_passes, but high blur_size (over around >
+    # if you want heavy blur, you need to up the blur_passes.
+    # the more passes, the more you can up the blur_size without noticing artifacts.
+}
+
+# Blur for waybar
+blurls=waybar
+
+animations {
+    enabled=1
+    # bezier=overshot,0.05,0.9,0.1,1.1
+    bezier=overshot,0.13,0.99,0.29,1.1
+    animation=windows,1,5,overshot,popin
+    animation=fade,1,10,default
+    animation=workspaces,1,6.5,overshot,slide
+    animation=border,1,10,default
+}
+
+dwindle {
+    pseudotile=1 # enable pseudotiling on dwindle
+    force_split=2
+}
+
+master {
+  new_on_top=true
+  no_gaps_when_only = true
+}
+
+misc {
+  disable_hyprland_logo=true
+  disable_splash_rendering=true
+  mouse_move_enables_dpms=true
+  #no_vfr=1
+}
+'';
   };
 }
